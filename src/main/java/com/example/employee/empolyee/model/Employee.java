dfb59ea7ca32ebba31e316ec08df122ecb6564fd
zabihi.b <zabihi.m@daneshrefah.ir>
2024-06-09 13:15:30 +0430

    git init

---
+++ src/main/java/com/example/employee/empolyee/model/Employee.java
@@ -0,0 +1,42 @@
+package com.example.employee.empolyee.model;
+
+
+public class Employee {
+
+    private String firstname;
+    private String lastname;
+    private String mobileNumber;
+    private String email;
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getMobileNumber() {
+        return mobileNumber;
+    }
+
+    public void setMobileNumber(String mobileNumber) {
+        this.mobileNumber = mobileNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
